400ff19ea578b5bdc5a9c0b70dce56d311018017
adonayewoldegebriel <adonaye@1964@gmail.com>
2026-05-20 18:17:08 -0500

    Add player search feature

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -1,12 +1,12 @@
 function PlayerCard({ player }) {
     return (
-      <div className="rounded-xl bg-slate-900 p-6 border border-slate-800">
+      <div className="rounded-xl bg-slate-900 p-6 border border-slate-800 hover:border-blue-500 transition">
         <h3 className="text-xl font-bold">
           {player.first_name} {player.last_name}
         </h3>
   
         <p className="text-slate-400 mt-2">
-          Team: {player.team.full_name}
+          Team: {player.team?.full_name || "Unknown"}
         </p>
   
         <p className="text-slate-400">
